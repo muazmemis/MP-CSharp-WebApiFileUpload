2494c2316fbe378c9b7074f9998ad89b337d6c7e
muaz52 <61652847+muaz52@users.noreply.github.com>
2021-03-27 12:53:31 +0300

    Add project files.

---
+++ WebApiFileUpload/Controllers/FileUploadsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using WebApiFileUpload.Models;
+
+namespace WebApiFileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public string Post([FromForm] FileUpload objectFile)
+        {
+            try
+            {
+                if (objectFile.files.Length>0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    using (FileStream fileStream=System.IO.File.Create(path+objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "Uploaded.";
+                    }
+                }
+                else
+                {
+                    return "Not Uploaded.";
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ WebApiFileUpload/Models/FileUpload.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebApiFileUpload.Models
+{
+    public class FileUpload
+    {
+        public IFormFile files { get; set; }
+    }
+}
